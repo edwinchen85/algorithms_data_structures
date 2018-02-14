8064e83670eeba14bf26fcca070fec1a6554e6e5
Edwin Chen <edwinchen85@gmail.com>
2018-02-15 02:48:16 +0800

    Solution 3

--- 13 Fibonacci/index.js
+++ 13 Fibonacci/index.js
@@ -21,12 +21,37 @@
 //   return result[n];
 // }
 
-function fib(n) {
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+
+//   return fib(n - 1) + fib(n - 2);
+// }
+
+function memoize(fn) {
+  const cache = {};
+
+  return function(...args) {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+
+    return result;
+  };
+}
+
+function slowFib(n) {
   if (n < 2) {
     return n;
   }
 
   return fib(n - 1) + fib(n - 2);
 }
+
+const fib = memoize(slowFib);
 
 module.exports = fib;
