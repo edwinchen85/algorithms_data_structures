7d53dfa90c0820ef22a690dce232d0b75b5236fc
Edwin Chen <edwinchen85@gmail.com>
2018-02-18 04:21:23 +0800

    ForEach

--- 18 Linked List/index.js
+++ 18 Linked List/index.js
@@ -137,6 +137,17 @@ class LinkedList {
     const node = new Node(data, previous.next);
     previous.next = node;
   }
+
+  forEach(fn) {
+    let node = this.head;
+    let counter = 0;
+
+    while (node) {
+      fn(node, counter);
+      node = node.next;
+      counter++;
+    }
+  }
 }
 
 module.exports = { Node: Node, LinkedList: LinkedList };
